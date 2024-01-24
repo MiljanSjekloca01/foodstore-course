6ec2ce5a7709dc2b1659a75e8ce7f50e5e54f811
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2024-01-24 15:16:51 +0100

    MongoDB added "First" Commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,58 +10,6 @@ app.use(cors({
     origin:["http://localhost:4200"]
 }));
 
-app.get("/api/foods", (req,res) => {
-    res.send(sample_foods);
-})
-
-app.get("/api/foods/search/:searchTerm", (req,res) => {
-    const searchTerm = req.params.searchTerm;
-    const foods =  sample_foods
-    .filter(food => food.name.toLowerCase()
-    .includes(searchTerm.toLowerCase()));
-    res.send(foods);
-})
-
-app.get("/api/foods/tags", (req,res) => {
-    res.send(sample_tags);
-})
-
-app.get("/api/foods/tag/:tagName", (req,res) => {
-    const tagName = req.params.tagName;
-    const foods = sample_foods
-    .filter(food => food.tags?.includes(tagName));
-    res.send(foods);
-})
-
-app.get("/api/foods/:foodId",(req,res) => {
-    const foodId = req.params.foodId;
-    const food = sample_foods
-    .find(food => food.id == foodId);
-    res.send(food);
-})
-
-app.post("/api/users/login", (req,res) => {
-    //Destracturing Assignent - Izvlacenje vrednosti iz objekta,..
-    //izvlacenje emaila i passworda iz req bodya
-    const {email,password} = req.body; 
-    const user = sample_users
-    .find(user => user.email === email &&
-        user.password === password);
-        //Ako user postoji saljemo odgovor.
-    if(user){
-        res.send(generateTokenResponse(user))
-    }else{
-        res.status(400).send("Email or password is not valid!");
-    }
-})
-
-const generateTokenResponse = (user:any) => {
-    const token = jwt.sign({
-        email: user.email, isAdmin: user.isAdmin
-    },"SomeSecretText", {
-        expiresIn:"30d"
-    });
-
     user.token = token;
     return user;
 }
