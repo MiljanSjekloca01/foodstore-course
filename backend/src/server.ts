2d37a75eb61faae80949b84e9ab6eb4fb12cfa4c
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2024-04-23 15:54:42 +0200

    server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,9 +22,9 @@ app.use("/api/users",userRouter);
 app.use("/api/orders",orderRouter);
 
 // Vidio kod njega
-app.use(express.static('frontend'));
+app.use(express.static('../../frontend/dist'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'frontend', 'index.html'))
+    res.sendFile(path.join(__dirname,'../../frontend/dist', 'index.html'))
 })
 
 // port koji koristimo,
